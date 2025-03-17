874eed3458938b12a6820567e4679782398b1d55
unahuli <40204903+unahuli@users.noreply.github.com>
2025-03-18 00:47:27 +0800

    Create playRound function
    Contains winning and losing conditions for the
    rock, paper and scissors game.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+humanScore = 0;
+computerScore = 0;
+
 function getComputerChoice() {
     randNumber = Math.floor(Math.random() * 3);
     if (randNumber === 0) {
@@ -9,11 +12,33 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 function getHumanChoice() {
     choice = prompt("Enter your move:");
     return choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === "rock" && computerChoice === "scissors") 
+        || (humanChoice === "paper" && computerChoice === "rock")
+        || (humanChoice === "scissors" && computerChoice === "paper")) {
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+    } else if ((computerChoice === "rock" && humanChoice === "scissors") 
+        || (computerChoice === "paper" && humanChoice === "rock")
+        || (computerChoice === "scissors" && humanChoice === "paper")) {
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+    } else {
+        console.log("It's a draw!")
+    }
+
+}
+
+humanChoice = getHumanChoice();
+computerChoice = getComputerChoice();
+console.log(computerChoice);
+console.log(playRound(humanChoice, computerChoice));
